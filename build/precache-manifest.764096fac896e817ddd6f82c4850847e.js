c2fb598307b43d6f3fd9e50f7f8859d014234f6f
tiavina-mika <ralainirinatiavina@gmail.com>
2020-04-12 12:07:08 +0300

    build

---
+++ build/precache-manifest.764096fac896e817ddd6f82c4850847e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427300bb1f0d502adf3d4b49a6478dda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1c8817679efe41c22d3",
+    "url": "/static/js/2.32613294.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.32613294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e71549695c81b344cc74",
+    "url": "/static/js/main.a276859d.chunk.js"
+  },
+  {
+    "revision": "6be0ba0430e2a3587ec6",
+    "url": "/static/js/runtime-main.83213f4f.js"
+  }
+]);
